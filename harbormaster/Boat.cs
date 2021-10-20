376fd34afce38f7d3210cfea8b54493f64d952d0
Abbegubbegum <albin.nojback@gmail.com>
2021-10-20 21:23:05 +0200

    Add Movement

--- harbormaster/Boat.cs
+++ harbormaster/Boat.cs
@@ -5,18 +5,23 @@ using Raylib_cs;
 
 public class Boat
 {
-    public Rectangle rec = new Rectangle(100, 400, 12, 24);
-    public Color c = Color.ORANGE;
-    private int triangleLength = 10;
+    public Vector2 center = new Vector2(100, 400);
+    public int r = 12;
+    private Color c = Color.ORANGE;
+    public int TriangleLenght { get; private set; } = 10;
+    public Vector2 dir = new Vector2(0, 0);
+    private float speed = 0.2f;
 
     public void Update()
     {
-
+        center.X += dir.X * speed;
+        center.Y += dir.Y * speed;
     }
 
     public void Draw()
     {
-        Raylib.DrawRectangleRec(rec, c);
-        Raylib.DrawTriangle(new Vector2(rec.x + rec.width, rec.y), new Vector2(rec.x + rec.width / 2, rec.y + triangleLength), new Vector2(rec.x, rec.y), c);
+        Raylib.DrawCircleV(center, r, c);
+        // Raylib.DrawRectangleRec(rec, c);
+        // Raylib.DrawTriangleLines(new Vector2(rec.x + rec.width, rec.y), new Vector2(rec.x + rec.width / 2, rec.y - TriangleLenght), new Vector2(rec.x, rec.y), Color.BLACK);
     }
 }
--- harbormaster/Program.cs
+++ harbormaster/Program.cs
@@ -2,27 +2,54 @@
 using System.Numerics;
 using Raylib_cs;
 
+//RAYLIB SHIT
 const int windowHeight = 1020;
 const int windowWidth = 800;
+Raylib.InitWindow(windowHeight, windowWidth, "Harbor Master");
+Raylib.SetTargetFPS(60);
+
+string gameState = "game";
 
 Boat b = new Boat();
 
+Rectangle target = new Rectangle(600, 600, 30, 30);
 
-Raylib.InitWindow(windowHeight, windowWidth, "Harbor Master");
+Vector2 mousePos;
 
 while (!Raylib.WindowShouldClose())
 {
-    //LOGIC
+    if (gameState == "game")
+    {
+        //LOGIC
+        mousePos = new Vector2(Raylib.GetMouseX(), Raylib.GetMouseY());
+        b.dir = Vector2.Normalize(mousePos - b.center) * 10;
+
+        b.Update();
+
+        if (Raylib.CheckCollisionCircleRec(b.center, b.r, target))
+        {
+            gameState = "end";
+        }
 
 
+        //DRAWING
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+        b.Draw();
+        Raylib.DrawRectangleRec(target, Color.DARKBLUE);
+        Raylib.DrawLineV(b.center, b.center + b.dir, Color.BLACK);
 
+        Raylib.EndDrawing();
+    }
+    else
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
 
+        Raylib.DrawText("END", 100, 100, 84, Color.BLACK);
 
-    //DRAWING
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.WHITE);
-    b.Draw();
-    Raylib.EndDrawing();
+        Raylib.EndDrawing();
+    }
 }
 
 
